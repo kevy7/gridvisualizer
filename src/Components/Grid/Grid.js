2c0cbb08e10ede6101e493df20c2f7bfacd0c7f1
kevy7 <kvnyang503@gmail.com>
2020-01-11 10:34:16 -0800

    created the createNode function

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -3,12 +3,43 @@ import React, {Component} from 'React';
 import Node from "../Node/Node";
 
 class Grid extends Component {
+    state = {
+        grid = []
+    }
+
+    componentWillMount = () => {
+        //let grid = createInitialGrid();
+        //this.setState({grid: grid})
+    }
+
     render(){
         return (
             <div>
                 This is the grid component
             </div>
         )
+    }
+
+    //Adding our functions down here
+    //We imported our nodes as well
+
+    //Create nodes and push them into our grid above
+
+    createInitialGrid = () => {
+        //We're going to create our grid here, and push in our node
+    }
+
+    //Create a node
+    createNode = (row, column) => {
+        //Return the following object
+        return {
+            row: row,
+            column: column,
+            startRow: undefined,
+            startColumn: undefined,
+            isVisited: false,
+            prevNode: undefined
+        }
     }
 }
 
