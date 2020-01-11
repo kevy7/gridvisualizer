61758fb67d0528081d3b33d26dc6609b051d200c
kevy7 <kvnyang503@gmail.com>
2020-01-10 16:24:46 -0800

    updated Node.js file to be a class-based component

---
+++ src/Components/Node/Node.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'React';
+
+class Node extends Component {
+    render(){
+        return (
+            <div className="Node">
+
+            </div>
+        )
+    }
+}
+
+export default Node;
