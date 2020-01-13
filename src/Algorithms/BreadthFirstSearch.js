592d8ca2e7512cc756832fe8aed96f2c50b8c5f1
kevy7 <kvnyang503@gmail.com>
2020-01-12 17:49:35 -0800

    updated BreadthFirstSearch component

--- src/Algorithms/BreadthFirstSearch.js
+++ src/Algorithms/BreadthFirstSearch.js
@@ -1,5 +1,24 @@
 
 //This is our breadth first search function
-export function BFS(){
+export function BFS(grid, startRow, startColumn, endRow, endColumn){
     
+    let startingNode = grid[startRow][startColumn]; //this will be our starting node
+    let endNode = [endRow][endColumn];
+
+    let queue = [];
+    //let visited = []; //these would be used if our nodes are not objects
+    //let prev = {}; //these would be used if our nodes are not objects
+
+    //array.push -> push an element in front of an array
+    //array.shift -> remove an element in front of an array
+
+    queue.push(startingNode);   //We're pushing our starting node into the queue
+
+    
+
+
+
+
+
+
 }
