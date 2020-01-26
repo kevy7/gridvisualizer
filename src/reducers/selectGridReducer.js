4b449a95cd7b230d9812578091718f3c542c340b
kevy7 <kvnyang503@gmail.com>
2020-01-25 23:28:24 -0800

    added import to top instead of middle of code for selectGridReducer.js

--- src/reducers/selectGridReducer.js
+++ src/reducers/selectGridReducer.js
@@ -1,9 +1,9 @@
+import { SELECT_START_GRID, SELECT_END_GRID } from '../actions/types';
+
 let initialState = {
     startingGrid: {},
     endingGrid: {}
 }
-
-import { SELECT_START_GRID, SELECT_END_GRID } from '../actions/types';
 
 const selectGridReducer = (state=initialState, action) => {
     
