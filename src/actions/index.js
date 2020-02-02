648784e219d9498f03672417b4b511cc946b3b99
kevy7 <kvnyang503@gmail.com>
2020-02-02 12:43:17 -0800

    added a comment to the action.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import {
     SELECT_ACTION
 } from './types';
 
+//select the type of action that the user wants
 export const selectAction = (userAction) => dispatch => {
     //If there is something in the user action, then pass it on
     if(userAction){
@@ -11,3 +12,4 @@ export const selectAction = (userAction) => dispatch => {
       });  
     }
 }  
+
