340022c8849d9cca1a318c69b1c0c3aa01f7f625
kevy7 <kvnyang503@gmail.com>
2020-02-06 20:28:14 -0800

    added a while loop to our DFS function

--- src/Algorithms/DebthFirstSearch.js
+++ src/Algorithms/DebthFirstSearch.js
@@ -4,6 +4,47 @@ export function DFS(grid, startRow, startCol, endRow, endCol){
     let maxRow = grid.length;
     let maxCol = grid[0].length;
     let startingNode = grid[startRow][startCol]; //This will be our starting node
-    
+    let stack = [];
+    let visited = []; //This will be an array of objects
+    /*
+        {
+            row: rowHere,
+            column: columnHere
+        }
+    */
+    //let shortestPath = []
+
+    /*
+        Data that each node contains    
+
+        row: row,
+        column: column,
+        startRow: undefined,
+        startColumn: undefined,
+        isVisited: false,
+        isPath: false,
+        prevNode: undefined,
+        isStart: false,
+        isEnd: false,
+    */
+
+    startingNode.isVisited = true;
+    stack.push({
+        row: startRow,
+        column: startCol
+    });
+
+    //While there is something in our stack, execute the following code below
+    while(stack.length){
+
+        let currentNode = stack.pop(); //Remove last element of your stack and assign it to the currentNode variable
+
+        
+
+    }
+
+
+
+
 
 }
