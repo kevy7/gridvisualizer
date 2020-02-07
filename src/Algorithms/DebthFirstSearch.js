f834a41a3e47000f0538480bd749552aa7e556e2
kevy7 <kvnyang503@gmail.com>
2020-02-06 20:11:47 -0800

    created the DFS algorithm

---
+++ src/Algorithms/DebthFirstSearch.js
@@ -0,0 +1,9 @@
+
+export function DFS(grid, startRow, startCol, endRow, endCol){
+
+    let maxRow = grid.length;
+    let maxCol = grid[0].length;
+    let startingNode = grid[startRow][startCol]; //This will be our starting node
+    
+
+}
