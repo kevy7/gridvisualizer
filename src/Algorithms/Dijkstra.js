ecc31722a56280f4312cd540fba236deacc66b4f
kevy7 <kvnyang503@gmail.com>
2020-02-12 21:04:27 -0800

    added variables to Dijkstra.js file

--- src/Algorithms/Dijkstra.js
+++ src/Algorithms/Dijkstra.js
@@ -1,7 +1,11 @@
 import { getChildrens, getShortestPath } from './Algofunctions/Algofunctions';
 
 const Dijkstra = (grid, startRow, startColoumn, endRow, endColumn) => {
-    
+    let maxRow = grid.length;
+    let maxCol = grid[0].length;
+    let startingNode = grid[startRow][startCol]; //This will be our starting node
+    //let stack = [];
+    let visited = []; //This will be an array of objects
 }
 
 export default Dijkstra;
