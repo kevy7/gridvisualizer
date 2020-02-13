8ccff6871d1cd208ecb4be0020c57e94aa4d37db
kevy7 <kvnyang503@gmail.com>
2020-02-12 22:19:49 -0800

    updated Dijstra.js file

--- src/Algorithms/Dijkstra.js
+++ src/Algorithms/Dijkstra.js
@@ -1,6 +1,6 @@
 import { getChildrens, getShortestPath } from './Algofunctions/Algofunctions';
 
-const Dijkstra = (grid, startRow, startColoumn, endRow, endColumn) => {
+const Dijkstra = (grid, startRow, startColumn, endRow, endColumn) => {
     let maxRow = grid.length;
     let maxCol = grid[0].length;
     let startingNode = grid[startRow][startCol]; //This will be our starting node
@@ -8,6 +8,15 @@ const Dijkstra = (grid, startRow, startColoumn, endRow, endColumn) => {
     let visited = []; //This will be an array of objects
 
     //First, we need to create some kind of data structure to order our nodes based on priority
+    startingNode.isVisited = true;
+    visited.push({
+        row: startRow,
+        column: startColumn
+    });
+    startingNode.distance = 0; //Set starting node's distance to 0
+
+
+
 
 }
 
