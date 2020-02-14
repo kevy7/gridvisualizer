dc6e1c96f888b06bd564bcee07367d1f9348a637
kevy7 <kvnyang503@gmail.com>
2020-02-13 21:48:14 -0800

    creating priorityQueue

--- src/Algorithms/Dijkstra.js
+++ src/Algorithms/Dijkstra.js
@@ -1,11 +1,14 @@
 import { getChildrens, getShortestPath } from './Algofunctions/Algofunctions';
+import priorityQueue from '../dataStructures/priorityQueue';
 
 const Dijkstra = (grid, startRow, startColumn, endRow, endColumn) => {
     let maxRow = grid.length;
     let maxCol = grid[0].length;
     let startingNode = grid[startRow][startColumn]; 
     //let priorityQueue = []; //using a binary heap instead
+    let queue = new priorityQueue();
     let visited = [];
+
 
     //console.log("yay");
     startingNode.distance = 0;
@@ -14,6 +17,13 @@ const Dijkstra = (grid, startRow, startColumn, endRow, endColumn) => {
         row: startingNode.row,
         column: startingNode.column
     });
+    grid[0][0].distance = 15;
+    grid[0][1].distance = 2;
+    queue.addQueue(startingNode);
+    queue.addQueue(grid[0][0]);
+    queue.addQueue(grid[0][1]);
+
+    console.log(queue.queue);
 
 
 
--- src/dataStructures/priorityQueue.js
+++ src/dataStructures/priorityQueue.js
@@ -3,13 +3,30 @@ class priorityQueue {
         this.queue = [] //We'll push our nodes into this queue
     }
 
-    addQueue = () => {
+    addQueue = (node) => {
+        this.queue.push(node);
 
+        //Bubble the node up based on priority, meaning distance
+        let currentIdx = this.queue.length - 1;
+        let currentNode = this.queue[currentIdx];
+
+        while(currentIdx > 0){
+            let parentIdx = Math.floor((currentIdx-1)/2);
+            let parent = this.queue[parentIdx];
+
+            if(currentNode.distance >= parent.distance) break;
+            this.queue[parentIdx] = currentNode;
+            this.queue[currentIdx] = parent;
+
+            currentIdx = parentIdx;
+        }
+        
     }
 
     deQueue = () => {
-
+        console.log("You executed this function!!!");
     }
-    
+
 }
 
+export default priorityQueue;
