71b1d0f2df7145c0357f1a0fda69b851e7bef9f6
kevy7 <kvnyang503@gmail.com>
2020-02-13 22:58:06 -0800

    updated the PriorityQueue.js file

--- src/dataStructures/priorityQueue.js
+++ src/dataStructures/priorityQueue.js
@@ -25,7 +25,46 @@ class priorityQueue {
     }
 
     deQueue = () => {
-        console.log("You executed this function!!!");
+        let firstPriority = this.queue[0];
+        let lastPriority = this.queue.pop(); //This should be trickled down
+
+        if(this.queue.length > 0){
+            this.queue[0] = lastPriority;
+
+            //Trickle down
+            this.sinkDown();
+        }
+    }
+
+    sinkDown = () => {
+        let currentIdx = 0;
+        let length = this.queue.length;
+        let currentNode = this.queue[0];
+
+        while(true){
+            //Find the current Node's childs
+            let leftidx = 2 * currentIdx + 1; //check if these are out of bounds
+            let rightidx = 2 * currentIdx + 2;
+            let left, right;
+            let swap = null;
+
+            if(leftidx < length){
+                left = this.queue[leftidx];
+                if(currentNode.distance > left){
+                    swap = leftidx;
+                }
+            }
+            if(rightidx < length){
+                right = this.queue[rightidx];
+                if(
+                    (currentNode.distance > right.distance && swap === null) 
+                    || 
+                    (swap !== null && right.distance < left.distance )){
+                        
+                }
+            }
+
+        }
     }
 
 }
