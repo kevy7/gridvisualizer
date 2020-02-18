fe23c27023261db9bdad9e49556df6454821ae6a
kevy7 <kvnyang503@gmail.com>
2020-02-17 22:22:36 -0800

    removed spacing

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -106,7 +106,6 @@ class NavBar extends Component {
                             </div>
 
                             <div className="navbar-item has-dropdown is-hoverable">
-
                                 <a className="navbar-link" >
                                     Algorithm
                                 </a>
