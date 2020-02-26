d5b52e5edc618ebe3bd867f60ee599f6eecde3b7
kevy7 <kvnyang503@gmail.com>
2020-02-25 23:32:46 -0800

    dijkstras alogrithm is not working the way i want it to

--- src/Algorithms/Dijkstra.js
+++ src/Algorithms/Dijkstra.js
@@ -76,7 +76,7 @@ const Dijkstra = (grid, startRow, startColumn, endRow, endColumn) => {
 
             //Add distance
             if(grid[childrens[a].row][childrens[a].column].isWeight){
-                grid[childrens[a].row][childrens[a].column].distance = 15 + grid[currentNode.row][currentNode.column].distance
+                grid[childrens[a].row][childrens[a].column].distance = 5 + grid[currentNode.row][currentNode.column].distance
             }
             else {
                 grid[childrens[a].row][childrens[a].column].distance = 1 + grid[currentNode.row][currentNode.column].distance
