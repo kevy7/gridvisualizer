209eb67f527c20faa24e15709dcdad61626dfbbb
kevy7 <kvnyang503@gmail.com>
2020-03-14 15:07:49 -0700

    added arguments to function

--- src/MazeAlgorithms/recursiveDivision.js
+++ src/MazeAlgorithms/recursiveDivision.js
@@ -1,5 +1,7 @@
 
-const recursiveDivision = () => {
+const recursiveDivision = (grid, startRow, startColumn, width, height, orientation) => {
+    //orientation determines whether we want to divide our graph horizantally or vertically first
+
     
 }
 
