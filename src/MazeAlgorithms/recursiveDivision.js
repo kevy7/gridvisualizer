16b8319110f9b49fe372bc70c41de64ae3f5ea9f
kevy7 <kvnyang503@gmail.com>
2020-03-14 21:28:15 -0700

    added notes to code

--- src/MazeAlgorithms/recursiveDivision.js
+++ src/MazeAlgorithms/recursiveDivision.js
@@ -3,7 +3,10 @@ const recursiveDivision = (grid, startRow, startColumn, width, height, orientati
     //orientation determines whether we want to divide our graph horizantally or vertically first
 
     //startRow and startColumn should always begin at (0,0)
+    let grid = grid;
+    let randomRow; //A random row will be selected to be our starting row. It has to be between 0 and maxWidth of our grid
     
+
 }
 
 export default recursiveDivision;
