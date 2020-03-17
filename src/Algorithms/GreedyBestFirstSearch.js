cb5d5b13736f5c3d257c74c9b26efc2c27a883a1
kevy7 <kvnyang503@gmail.com>
2020-03-16 21:06:09 -0700

    added arguments to function

--- src/Algorithms/GreedyBestFirstSearch.js
+++ src/Algorithms/GreedyBestFirstSearch.js
@@ -4,7 +4,7 @@ import priorityQueue from '../dataStructures/priorityQueue';
 //My priorityQurue data structure should work with this algorithm, we want to visit the node that has a 
 //lower estimated distance to the end
 
-const GreedyBestFirstSearch = () => {
+const GreedyBestFirstSearch = (grid, startRow, startColumn, endRow, endColumn) => {
 
 }
 
