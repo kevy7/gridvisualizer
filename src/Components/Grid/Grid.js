a79031b69334cf6f9d98180758f72725787fc56b
kevy7 <kvnyang503@gmail.com>
2020-03-18 23:47:34 -0700

    updated speed for alogirithms

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -141,7 +141,7 @@ class Grid extends Component {
 
                     document.getElementById(`node-${node.row}-${node.column}`).className = "node node-visited";
 
-                }, index * 10);
+                }, index * 15);
             });
 
             algoResult.shortestPath.reverse(); //to reverse the shortestPath
@@ -150,7 +150,7 @@ class Grid extends Component {
                 index+=1;
                 setTimeout(() => {
                     document.getElementById(`node-${node.row}-${node.column}`).className = "node node-path";
-                }, index * 10.5);
+                }, index * 16); //10.5
             })
 
             }//try
