0cf858294cb995179c5f1dcc7f70dfb47545e4db
kevy7 <kvnyang503@gmail.com>
2020-03-18 23:18:09 -0700

    added a new userAlgo type

--- src/userAlgo/userAlgo.js
+++ src/userAlgo/userAlgo.js
@@ -1,3 +1,4 @@
 export const BreadthFS = "Breadth-First-Search";
 export const DebthFS = "Debth-First-Search";
 export const DijkstraAlgo = "Dijkstra's Algorithm";
+export const GreedyBFS = "Greedy Best-First-Search";
