b9162702586545ff7c4ac9a53e0df595e5589572
kevy7 <kvnyang503@gmail.com>
2020-03-20 22:00:16 -0700

    created a new function

---
+++ src/functions/ifContainsObject.js
@@ -0,0 +1,11 @@
+/*
+    Purpose of this function. It will take in an array and object. If the object is already within the array
+    then output it as true and stop the function.
+*/
+const ifContainsObject = (ArrayObjects, objectVal) => {
+    let ifContains = false;
+
+    
+}
+
+export default ifContainsObject;
