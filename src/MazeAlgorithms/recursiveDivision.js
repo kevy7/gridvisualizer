8d7952023eabc8a4eaa6a2b963dc20ca2166778e
kevy7 <kvnyang503@gmail.com>
2020-03-23 22:11:01 -0700

    updated the recursiveDivision function

--- src/MazeAlgorithms/recursiveDivision.js
+++ src/MazeAlgorithms/recursiveDivision.js
@@ -1,11 +1,10 @@
 
-const recursiveDivision = (grid, startRow, startColumn, width, height, orientation) => {
+const recursiveDivision = (grid, width, height, orientation) => {
     //orientation determines whether we want to divide our graph horizantally or vertically first
 
-    //startRow and startColumn should always begin at (0,0)
-    let grid = grid;
-    let randomRow; //A random row will be selected to be our starting row. It has to be between 0 and maxWidth of our grid
-    
+    //This is going to be our helper function, we will create a recursive function, that will add walls into the array below
+    let mazeWalls = []; //Set this as an empty array for now
+
 
 }
 
