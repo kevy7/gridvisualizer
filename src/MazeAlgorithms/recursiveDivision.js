1e7762a6ca1fdaf9c74617edd92cd04996fdc9c5
kevy7 <kvnyang503@gmail.com>
2020-03-23 22:18:19 -0700

    added comments

--- src/MazeAlgorithms/recursiveDivision.js
+++ src/MazeAlgorithms/recursiveDivision.js
@@ -1,9 +1,14 @@
 
 const recursiveDivision = (grid, width, height, orientation) => {
     //orientation determines whether we want to divide our graph horizantally or vertically first
+    //width === columns
+    //height === rows
 
     //This is going to be our helper function, we will create a recursive function, that will add walls into the array below
     let mazeWalls = []; //Set this as an empty array for now
+
+    //First of wall, execute a function that will loop through all the outer edges of your current graph and display them as walls
+
 
 
 }
