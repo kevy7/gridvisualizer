08108b6594ee6b8b1fa617e5dfa6ed87f42af84d
kevy7 <kvnyang503@gmail.com>
2020-04-02 20:22:12 -0700

    Updates

---
+++ precache-manifest.231fefc12586048b34a92c26f3ffcb38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54fd01f808ab66646e4ceaaccd78255",
+    "url": "/gridvisualizer/index.html"
+  },
+  {
+    "revision": "9bd5008ab12bf583a72e",
+    "url": "/gridvisualizer/static/css/2.a41c9227.chunk.css"
+  },
+  {
+    "revision": "fb09de1a67f1bab6277b",
+    "url": "/gridvisualizer/static/css/main.60d5fedf.chunk.css"
+  },
+  {
+    "revision": "9bd5008ab12bf583a72e",
+    "url": "/gridvisualizer/static/js/2.30ad4736.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gridvisualizer/static/js/2.30ad4736.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb09de1a67f1bab6277b",
+    "url": "/gridvisualizer/static/js/main.fcb14262.chunk.js"
+  },
+  {
+    "revision": "4466ee75d2fc23825d5d",
+    "url": "/gridvisualizer/static/js/runtime-main.c62e8a6a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gridvisualizer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gridvisualizer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gridvisualizer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gridvisualizer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gridvisualizer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gridvisualizer/precache-manifest.231fefc12586048b34a92c26f3ffcb38.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gridvisualizer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
