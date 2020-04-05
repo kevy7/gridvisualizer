4070ea6e64059cd7e56bca4dd1a8949590566060
kevy7 <kvnyang503@gmail.com>
2020-04-04 18:23:59 -0700

    created a new reducer for our grid

---
+++ src/reducers/selectMazeAlgoReducer.js
@@ -0,0 +1,11 @@
+import { SELECT_MAZE_ALGO } from "../actions/types";
+
+initialState = {
+    MazeAlgorithem: ""
+}
+
+const selectMazeAlgoReducer = (state=initialState, action) => {
+
+}
+
+export default selectMazeAlgoReducer;
