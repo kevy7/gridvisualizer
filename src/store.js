04901e695d773a915fbbe9436a30546fc5395289
kevy7 <kvnyang503@gmail.com>
2020-04-06 19:39:43 -0700

    re added redux settings

--- src/store.js
+++ src/store.js
@@ -10,10 +10,10 @@ const middleware = [thunk];
 export const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(...middleware)
+    //applyMiddleware(...middleware)
 
-    /* compose(
+    compose(
         applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ) */
+    )
 );
