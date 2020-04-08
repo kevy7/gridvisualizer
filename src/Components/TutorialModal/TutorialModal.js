14285aea06c59c2ebe825b447300afcb699cd75d
kevy7 <kvnyang503@gmail.com>
2020-04-07 20:17:00 -0700

    created the tutorial modal

---
+++ src/Components/TutorialModal/TutorialModal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TutorialModal {
+
+    render(){
+        return (
+            <div className="tutorialModal">
+
+            </div>
+        )
+    }
+}
+
+export default TutorialModal;
