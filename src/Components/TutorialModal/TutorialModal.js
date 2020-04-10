ebce1b7f45aa6990ecfa36f6363f21d5274d014f
kevy7 <kvnyang503@gmail.com>
2020-04-09 22:03:19 -0700

    added modal elements to the tutorialModal

--- src/Components/TutorialModal/TutorialModal.js
+++ src/Components/TutorialModal/TutorialModal.js
@@ -6,7 +6,22 @@ class TutorialModal {
     render(){
         return (
             <div className="tutorialModal">
-
+                <div className="modal">
+                <div className="modal-background"></div>
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                    <p className="modal-card-title">Modal title</p>
+                    <button className="delete" aria-label="close"></button>
+                    </header>
+                    <section className="modal-card-body">
+                        
+                    </section>
+                    <footer className="modal-card-foot">
+                    <button className="button is-success">Save changes</button>
+                    <button className="button">Cancel</button>
+                    </footer>
+                </div>
+                </div>
             </div>
         )
     }
