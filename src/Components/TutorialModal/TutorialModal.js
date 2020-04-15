eddc1d9441f52858286ab64bdd418c766f292304
kevy7 <kvnyang503@gmail.com>
2020-04-14 21:41:13 -0700

    made the modal active

--- src/Components/TutorialModal/TutorialModal.js
+++ src/Components/TutorialModal/TutorialModal.js
@@ -6,21 +6,21 @@ class TutorialModal extends Component {
     render(){
         return (
             <div className="tutorialModal">
-                <div className="modal">
-                <div className="modal-background"></div>
-                <div className="modal-card">
-                    <header className="modal-card-head">
-                    <p className="modal-card-title">Modal title</p>
-                    <button className="delete" aria-label="close"></button>
-                    </header>
-                    <section className="modal-card-body">
-                        
-                    </section>
-                    <footer className="modal-card-foot">
-                    <button className="button is-success">Save changes</button>
-                    <button className="button">Cancel</button>
-                    </footer>
-                </div>
+                <div className="modal is-active">
+                    <div className="modal-background"></div>
+                    <div className="modal-card">
+                        <header className="modal-card-head">
+                        <p className="modal-card-title">Modal title</p>
+                        <button className="delete" aria-label="close"></button>
+                        </header>
+                        <section className="modal-card-body">
+                            
+                        </section>
+                        <footer className="modal-card-foot">
+                        <button className="button is-success">Save changes</button>
+                        <button className="button">Cancel</button>
+                        </footer>
+                    </div>
                 </div>
             </div>
         )
