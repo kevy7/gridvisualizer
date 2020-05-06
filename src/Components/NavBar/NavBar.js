5c06cac794a78e21d68d69b98a4cac77b41663e2
kevy7 <kvnyang503@gmail.com>
2020-05-05 19:55:38 -0700

    removed Bulma logo from my app

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -73,9 +73,11 @@ class NavBar extends Component {
                 {/*<TutorialModal />*/}
                 <nav className="navbar is-transparent">
                     <div className="navbar-brand">
+                        {/*
                         <a className="navbar-item" href="https://bulma.io">
                             <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
                         </a>
+                        */}
                         <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
                             <span></span>
                             <span></span>
