ec915dc55fe6577b3c9d6339649d1afb428b885a
kevy7 <kvnyang503@gmail.com>
2020-05-09 19:45:11 -0700

    updated modal section 1

--- src/Components/TutorialModal/ModalSections/ModalSections.js
+++ src/Components/TutorialModal/ModalSections/ModalSections.js
@@ -5,19 +5,23 @@ class ModalSections extends Component {
     render(){
         return (
             <div className="content">
-                <h2>Title</h2>
-                <p>Hi welcome to my pathfinding visualization app that was heavily inspired by Clément Mihailescu. With this website, you have three main tabs to use below.</p>
+                {/*<h2>Title</h2>*/}
+                <p>Hi, welcome to my pathfinding visualization app that was heavily inspired by Clément Mihailescu. With this website, you have three main tabs to use below.</p>
                 {/* Place image here */}
 
                 <ul>
                     <li>Action</li>
                     <ul>
-                        <li>Start</li>
-                        <li>End</li>
-                        <li>Traffic</li>
-                        <li>Wall</li>
+                        <li>Your list of available actions to choose from. i.e. Start action is used to select a starting node.</li>
                     </ul>
-                    <li>item 2</li>
+                    <li>Algorithms</li>
+                    <ul>
+                        <li>Contains the list of available pathfinding algorithms to choose.</li>
+                    </ul>
+                    <li>Maze Algorithms</li>
+                    <ul>
+                        <li>Contains the list of available maze generation algorithms to choose.</li>
+                    </ul>
                 </ul>
 
             </div>
