f7f8a50e551e913c0c60f1fb7077cfb401bb502d
kevy7 <kvnyang503@gmail.com>
2020-05-13 21:32:31 -0700

    added paragraph to the modalSections2

--- src/Components/TutorialModal/ModalSections/ModalSections2.js
+++ src/Components/TutorialModal/ModalSections/ModalSections2.js
@@ -6,7 +6,14 @@ class ModalSections2 extends Component {
             <div className="sections">
                 <div className="content">
                     <h3>Getting started</h3>
-
+                    <p>The defualt action will always be the start action. 
+                        First, select and press on any square in the grid. 
+                        After a square is selected, you will see the start icon displayed on the grid. 
+                        Now, after selecting a starting square, your action will automatically switch to the stop action. 
+                        Select any square to be your end destination. You will then see a stop icon displayed on your selected square.
+                    </p>
+                    {/* Place tutorial image here */}
+                    
                 </div>
             </div>
         )
