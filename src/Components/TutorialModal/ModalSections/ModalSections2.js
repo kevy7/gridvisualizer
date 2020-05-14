af1ffba722f536e33550841661f3ff9b1f19bc04
kevy7 <kvnyang503@gmail.com>
2020-05-13 21:28:08 -0700

    switched from h1 to h3

--- src/Components/TutorialModal/ModalSections/ModalSections2.js
+++ src/Components/TutorialModal/ModalSections/ModalSections2.js
@@ -5,7 +5,7 @@ class ModalSections2 extends Component {
         return (
             <div className="sections">
                 <div className="content">
-                    <h1>Getting started</h1>
+                    <h3>Getting started</h3>
 
                 </div>
             </div>
