4771849ce25d1356fcb1dc7635d1742de6b9702a
kevy7 <kvnyang503@gmail.com>
2020-05-30 22:47:04 -0700

    added render code

--- src/Components/TutorialModal/ModalSections/ModalSections3.js
+++ src/Components/TutorialModal/ModalSections/ModalSections3.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class ModalSections3 extends Component {
-
+    render(){
+        return (
+            <div className="modalSection3">
+                
+            </div>
+        )
+    }
 }
 
 export default ModalSections3;
