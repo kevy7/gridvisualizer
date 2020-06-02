07484123491ecdec2816ecff7c65e80abc765016
kevy7 <kvnyang503@gmail.com>
2020-06-01 23:12:49 -0700

    added details for walls and traffic nodes

--- src/Components/TutorialModal/ModalSections/ModalSections3.js
+++ src/Components/TutorialModal/ModalSections/ModalSections3.js
@@ -7,7 +7,13 @@ class ModalSections3 extends Component {
                 <div className="content">
 
                     <h3>Selecting Walls and Wieghted Nodes (traffic)</h3>
-                    
+
+                    <p>Selecting walls will stop an algorithm from using nodes as a path and can encourge alternative routes.</p>
+                    <p>The algorithms in this web app will be smart enough to avoid walls.</p>
+
+                    <p>Selecting weighted nodes (traffi), will encourage weigthed algorithms to avoid high traffic nodes.</p>
+                    <p>Unlike walls, alrogithms can still use traffic nodes as a path, but only if it leads to the shortest possible path.</p>
+
                 </div>
 
             </div>
