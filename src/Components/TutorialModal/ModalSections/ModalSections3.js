262151cc00ff90f50b86367a2ecfcd6d811650f1
kevy7 <kvnyang503@gmail.com>
2020-06-04 22:37:37 -0700

    changed classname for modal3, so that it will auto-hide itself

--- src/Components/TutorialModal/ModalSections/ModalSections3.js
+++ src/Components/TutorialModal/ModalSections/ModalSections3.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ModalSections3 extends Component {
     render(){
         return (
-            <div className="sections">
+            <div className="sections hide-section">
                 <div className="content">
 
                     <h3>Selecting Walls and Wieghted Nodes (traffic)</h3>
