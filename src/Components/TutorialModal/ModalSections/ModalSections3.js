b3a3cab505def5846eff6a19c9c5dbeeb43ab1bc
kevy7 <kvnyang503@gmail.com>
2020-06-08 21:28:18 -0700

    added a couple more tips and tricks to modal 3

--- src/Components/TutorialModal/ModalSections/ModalSections3.js
+++ src/Components/TutorialModal/ModalSections/ModalSections3.js
@@ -7,11 +7,15 @@ class ModalSections3 extends Component {
                 <div className="content">
 
                     <h3>Selecting Walls and Wieghted Nodes (traffic)</h3>
-                    <p>Selecting walls will stop an algorithm from using nodes as a path and can encourage alternative routes.</p>
-                    
+                    <p>Selecting walls will stop an algorithm from using nodes as a path and can encourage alternative routes. Follow the steps below to select walls.</p>
+                    <ul>
+                        <li>Select the Wall action.</li>
+                        <li>Then, hover over to your any nodes and press/hold down the 'e' key. While hovering, nodes will be selected as walls.</li>
+                    </ul>
 
-                    <p>Selecting weighted nodes (traffic), will encourage weighted algorithms to avoid those nodes.</p>
-                    <p>Unlike walls, alrogithms can still use traffic nodes as a path but only if it leads to the shortest possible path.</p>
+                    <p>Selecting weighted nodes (traffic), will encourage weighted algorithms to avoid those nodes.
+                    Unlike walls, alrogithms can still use traffic nodes as a path but only if it leads to the shortest possible path.
+                    </p>
 
                 </div>
 
