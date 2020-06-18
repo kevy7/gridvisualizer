a1a43c71ca3a65328cff679327ea885b298b9e6c
kevy7 <kvnyang503@gmail.com>
2020-06-17 20:51:03 -0700

    updated recursive backtracking

--- src/MazeAlgorithms/recursiveBacktracing.js
+++ src/MazeAlgorithms/recursiveBacktracing.js
@@ -1,5 +1,6 @@
 const recursiveBacktracking = () => {
     //Find a way to return walls, issue is we can create paths but how would we return the walls?
+    
 }
 
 export default recursiveBacktracking;
