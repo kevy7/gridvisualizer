2845d49d7526ee734b85e2f09f7875dd9aee1aa0
kevy7 <kvnyang503@gmail.com>
2020-06-18 21:56:42 -0700

    created a function to get children

--- src/MazeAlgorithms/recursiveBacktracing.js
+++ src/MazeAlgorithms/recursiveBacktracing.js
@@ -1,6 +1,11 @@
 const recursiveBacktracking = (grid, minWidth, maxWidth, minHeight, maxHeight) => {
     //Find a way to return walls, issue is we can create paths but how would we return the walls?
-    
+
 }
 
 export default recursiveBacktracking;
+
+//create a function here called get children
+const getChildren = () => {
+    
+}
