b10f10cb504a4ce3aed5b9a1661f2fed2c4e77fa
kevy7 <kvnyang503@gmail.com>
2020-06-18 22:41:32 -0700

    setup variables for recursiveBacktracking function

--- src/MazeAlgorithms/recursiveBacktracing.js
+++ src/MazeAlgorithms/recursiveBacktracing.js
@@ -1,8 +1,16 @@
 const recursiveBacktracking = (grid, minWidth, maxWidth, minHeight, maxHeight) => {
     //Find a way to return walls, issue is we can create paths but how would we return the walls?
     //ideally, function should stop when every node was visited
+    let stack = [];
+    let mazeWalls = [];
+    let randomRow = Math.floor(Math.random() * (maxHeight - minHeight)) + minHeight; //Will select a random row within range
+    let randomColumn = Math.floor(Math.random() * (maxWidth - minWidth)) + minWidth; //Will select a random column within range
 
     
+
+
+    //Math.floor(Math.random() * 10-5) + 5; //Used to return a random number from 5 and 10
+
 }
 
 export default recursiveBacktracking;
