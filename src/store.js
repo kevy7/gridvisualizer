8d82ebeee4f2b233d66be43950155c96480bc3a2
kevy7 <kvnyang503@gmail.com>
2020-07-08 21:52:00 -0700

    updated code to push

--- src/store.js
+++ src/store.js
@@ -10,10 +10,12 @@ const middleware = [thunk];
 export const store = createStore(
     reducers,
     initialState,
-    //applyMiddleware(...middleware)
+    applyMiddleware(...middleware)
 
+    /*
     compose(
         applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
+    */
 );
